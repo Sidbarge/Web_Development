dcf4aee5ddb3162300d7491043faccbbd16a1be8
Siddharth <siddharthbarge123@gmail.com>
2024-02-07 03:25:21 +0530

    Added more code

--- JavaScript/Dynamic_website_Builder/script.js
+++ JavaScript/Dynamic_website_Builder/script.js
@@ -23,7 +23,33 @@ function createCard(title, cName, views, monthsOld, duration, thumbnail){
 
     let MO=document.getElementsByClassName("monthsold")
     MO[0].innerHTML=monthsOld
+    
+//     let viewStr
+//     if (views < 1000) {
+//         viewStr = views;
+//     }
+//     else if (views > 1000000) {
+//         viewStr = views / 1000000 + "M";
+//     }
+//     else {
+//         viewStr = views / 1000 + "K";
+//     }
+//     let html = `<div class="card">
+//     <div class="image">
+//         <img src="${thumbnail}"
+//             alt="">
+//         <div class="capsule">${duration}</div>
+//     </div>
+//     <div class="text">
+//         <h1>${title}</h1>
+//         <p>${cName} . ${viewStr} views . ${monthsOld} months ago</p>
+//     </div> 
+// </div>`
+
+//     document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + html
 }
+
+
 
 
 createCard("Installing VS Code & How Websites Work | Sigma Web Development Course - Tutorial #1", "CodeWithHarry", 560000, "7 months ago", "31:20", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
